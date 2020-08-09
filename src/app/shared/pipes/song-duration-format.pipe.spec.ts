b7689fa45164594505d141afcfa1e3c7466501e3
Sergiu <szatmari.sergiu@gmail.com>
2020-08-09 18:50:55 +0300

    Removed *.spec.ts files; Renamed all refs to project's old name

--- src/app/shared/pipes/song-duration-format.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SongDurationFormatPipe } from './song-duration-format.pipe';
-
-describe('SongDurationFormatPipe', () => {
-  it('create an instance', () => {
-    const pipe = new SongDurationFormatPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
