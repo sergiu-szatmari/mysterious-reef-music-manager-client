04dc2ba49d74641bb3c41d5d752121db2f8f9e8d
Sergiu <szatmari.sergiu@gmail.com>
2020-09-18 16:56:27 +0300

    Min

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseURL: 'https://mysterious-reef-17305.herokuapp.com/api',
+  baseURL: '',
   secretMessage: 'This is a secret message coming from production environment'
 };
